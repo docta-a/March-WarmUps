f1dbad1d36b804f18f10faae9d2d72914b46c4d7
Game110AM <Game110AM@605-110-STU20>
2017-03-02 09:49:25 -0700

    hi

---
+++ march 1st/march 1st/march 1sr.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+char input;
+void Jukebox(char input);
+
+int main() {
+
+	Jukebox(input);
+
+}
+
+void Jukebox(char input) {
+	cout << "Dog (g), Cat (c), Pig (p) " << endl;
+	cin >> input;
+	switch (input) {
+	case 'd':
+		cout << "d";
+		PlaySound("dog.wav", NULL, SND_FILENAME);
+		break;
+	case 'c':
+		cout << "c";
+		PlaySound("cat.wav", NULL, SND_FILENAME);
+		break;
+	case 'p':
+		cout << "p";
+		PlaySound("pig.wav", NULL, SND_FILENAME);
+		break;
+	}
+}
